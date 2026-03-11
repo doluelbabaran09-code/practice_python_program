bfa4c5a6ef505bf8d7522b68366ed1a8670b9d95
doluelbabaran09-code <doluelbabaran09@gmail.com>
2026-03-11 13:43:29 +0800

    Delete 6.py

--- 6.py
+++
@@ -1,9 +0,0 @@
-# Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
-numbers = {}
-for i in range(10):
-    num = float(input(f"Enter number {i + 1}: "))
-    numbers[i] = num
-result = numbers[0]
-for i in range(1, 10):
-    result -= numbers[i]
-    print(f"The result of the first number minus all of the remaining numbers is: {result}")
