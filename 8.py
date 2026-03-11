2ef79a440a28d957ff703728d991945ee5ed0c65
doluelbabaran09-code <doluelbabaran09@gmail.com>
2026-03-11 13:43:45 +0800

    Delete 8.py

--- 8.py
+++
@@ -1,8 +0,0 @@
-# Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
-number = 0
-while number <= 100:
-    if number % 2 != 0:
-        print(number)
-        number += 1
-    else:
-        number += 1
