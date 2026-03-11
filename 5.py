efc24a9d643e64eb7f3f776dfbd47ee3a4de2744
doluelbabaran09-code <doluelbabaran09@gmail.com>
2026-03-11 13:43:21 +0800

    Delete 5.py

--- 5.py
+++
@@ -1,6 +0,0 @@
-# Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
-
-num_1 = float(input("Enter the first number: "))
-num_2 = float(input("Enter the second number: "))
-remainder_of_numbers = num_1 % num_2
-print(f"The remainder when the first number is divided by the second number is: {remainder_of_numbers}")
