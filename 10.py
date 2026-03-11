32958298360f24853bafd465ed7313011b2b6d18
doluelbabaran09 <dolueelbabaran09@gmail.com>
2026-03-11 13:39:17 +0800

    Cleanup: removed numbered practice scripts

--- 10.py
+++
@@ -1,9 +0,0 @@
-# Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
-num_1 = float(input(("Enter the first number:")))
-num_2 = float(input("Enter the second number: "))
-if num_1 < num_2:
-    for i in range(int(num_1) + 1, int(num_2)):
-        print(i)
-else:
-    for i in range(int(num_2) +1, int(num_1)):
-        print(i)
--- 3.py
+++
@@ -1,7 +0,0 @@
-# Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
-
-num_1 = float(input("Enter the first number:"))
-num_2 = float(input("Enter the second number:"))
-
-difference_of_numbers = num_1 - num_2
-print(f"The difference of the two numbers is: {difference_of_numbers}")
